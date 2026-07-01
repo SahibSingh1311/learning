81d84ec3f68d98fb2ef38c784f0936a3e5648856
Sahib Singh <padamsahibsingh@gmail.com>
2026-07-01 18:10:45 +0530

    Radio Button Integrated

--- app/src/main/java/com/learnandroid/learnradiobuttons/MainActivity.kt
+++ app/src/main/java/com/learnandroid/learnradiobuttons/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.learnandroid.learnradiobuttons
 
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.learnandroid.learnradiobuttons.ui.theme.LearnRadioButtonsTheme
+import kotlinx.coroutines.selects.select
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearnRadioButtonsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ShowRadioButtons(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +47,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ShowRadioButtons(modifier: Modifier = Modifier) {
+    val options = listOf("Option 1", "Option 2", "Option 3")
+    var selectedOption by remember { mutableStateOf<String?>(null) }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        options.forEach { option ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .selectable(
+                        selected = option == selectedOption,
+                        onClick = { selectedOption = option },
+                        role = Role.RadioButton
+                    )
+            ) {
+                RadioButton(selected = (option == selectedOption),
+                    onClick = null)
+                Spacer(modifier = Modifier.padding(8.dp))
+                Text(option)
+            }
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearnRadioButtonsTheme {
-        Greeting("Android")
+        Text("Selected Option: ${selectedOption ?: "None"}")
     }
 }
